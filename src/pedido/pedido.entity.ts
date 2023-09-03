1cfef5d883d11b846fa4a85b1a7053bba724a02d
Dieimp <mraingue@gmail.com>
2023-09-03 14:00:58 -0300

    torca de valor default para o active estava crashando a aplicao

---
+++ src/pedido/pedido.entity.ts
@@ -0,0 +1,51 @@
+import { Int,Field, ObjectType } from "@nestjs/graphql"
+import { User } from "src/user/user.entity";
+// import { type } from "os"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+
+@ObjectType()
+@Entity()
+export class Pedido{
+
+    @PrimaryGeneratedColumn()
+    @Field(type => Int)
+    id: number;
+
+    @Column({
+        type: "int",
+        unique: false,
+        nullable: false
+    })
+    @Field(type => Int)
+    client_Id: number;
+
+    @Column({
+        type: "datetime",
+        unique: false,
+        nullable: false,
+
+    })
+    @Field()
+    dt_Insert: Date;
+
+    @Column({
+        type: "datetime",
+        unique: false,
+        nullable: true, 
+    })
+    @Field()
+    dt_Conclusion: Date;
+
+    @Column({
+        type: "float",
+        unique: false,
+        nullable: true
+    })
+    @Field()
+    total_Value: number;
+
+    @ManyToOne(() => User, user => user.pedidos)
+    @Field(type => User)
+    user: User
+
+}
